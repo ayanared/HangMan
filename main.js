d96d156feac162e1762f47d8b4ccbddf904e95c2
ayanared <ayanared.gmail.com>
2017-12-12 17:00:18 -0500

    added a didIWin function and a didILose function

--- main.js
+++ main.js
@@ -1,51 +1,60 @@
 const GameBoard = {
-    wordToGuess : "Hello",
+    wordToGuess: "",
     wordToGuessArray: [],
-    wasGuessCorrect : false,
-    correctGuessesMade : [],
-    guessesLeft : 6,
-    startGame : function(){
-        const randomIndex = Math.floor(Math.random()*answers.length);
+    wasGuessCorrect: false,
+    correctGuessesMade: [],
+    guessesLeft: 6,
+    wonGame : false,
+    lostGame : false,
+    startGame: function () {
+        const randomIndex = Math.floor(Math.random() * answers.length);
         this.wordToGuess = answers[randomIndex].word.toLowerCase();
         this.wordToGuessArray = this.wordToGuess.split("");
-        for (let i = 0; i<this.wordToGuessArray.length; i++){
+        for (let i = 0; i < this.wordToGuessArray.length; i++) {
             this.correctGuessesMade.push('_');
         }
     },
-    getGuess : function(letter) {
+    getGuess: function (letter) {
         //get guess from user
         let indexOfLetterGuessed = this.wordToGuessArray.indexOf(letter);
-        if(indexOfLetterGuessed > -1){
+        if (indexOfLetterGuessed > -1) {
             this.wasGuessCorrect = true;
-            while(indexOfLetterGuessed >-1){
-                this.correctGuessesMade.splice(indexOfLetterGuessed,1,letter)
-                GameBoard.wordToGuessArray.splice(indexOfLetterGuessed, 1,"-");
+            while (indexOfLetterGuessed > -1) {
+                this.correctGuessesMade.splice(indexOfLetterGuessed, 1, letter)
+                GameBoard.wordToGuessArray.splice(indexOfLetterGuessed, 1, "-");
                 indexOfLetterGuessed = this.wordToGuessArray.indexOf(letter);
             }
-            
+
         }
-        else{
+        else {
             //if incorrect: 1. put letter in incorrectGuessesMade array
-            console.log(`${letter} was not there!`)
             this.wasGuessCorrect = false;
-            this.guessesLeft --;
-            console.log("guesses left:" + this.guessesLeft)
-
-
+            this.guessesLeft--;
         }
-        
+
+    },
+    didIWin: function(){
+        if (this.wordToGuess===this.correctGuessesMade.join("")){
+            this.wonGame = true;
+        }
+    },
+    didILose: function(){
+        if(this.guessesLeft === 0){
+            this.lostGame = true;
+        }
     }
-    
+
+
 
 }
 
 const ViewEngine = {
-    setUpBoard : function() {
+    setUpBoard: function () {
         //put blanks
         const blanks = GameBoard.correctGuessesMade.join("");
         $(guessing_word).text(blanks);
     },
-    getGuessFromUser: function(id){
+    getGuessFromUser: function (id) {
         //gray out button that has been guessed already
         $(`#${id}`).removeClass('unclicked');
         $(`#${id}`).addClass('clicked');
@@ -53,22 +62,19 @@ const ViewEngine = {
         $(`#${id}`).off('click');
         //
     },
-    updateWordToGuess : function() {
+    updateWordToGuess: function () {
         //display letters guessed correctly
-        
+
         const wordGuessed = GameBoard.correctGuessesMade.join("")
-        console.log(wordGuessed)
         $(guessing_word).text(wordGuessed);
-        console.log('updateWordToGuess')
 
     },
-    updateHangMan : function() {
+    updateHangMan: function () {
         //increase hangman parts
     },
-    updateNumberToGuess: function(){
+    updateNumberToGuess: function () {
         //update guesses
-        console.log("did this happen?")
-        
+
         $('#guesses_left').text(GameBoard.guessesLeft);
 
 
@@ -78,69 +84,74 @@ const ViewEngine = {
 }
 
 const AppController = {
-    handleBoardSetUp: function(){
+    handleBoardSetUp: function () {
         //GameBoard.startGame
         GameBoard.startGame();
         //set up gameboard view
         ViewEngine.setUpBoard();
 
     },
-    handleMakeGuess: function(event){
-        const letterGuessed = ($(event.target).attr('id'));  
+    handleMakeGuess: function (event) {
+        const letterGuessed = ($(event.target).attr('id'));
         GameBoard.getGuess(letterGuessed);
         ViewEngine.getGuessFromUser(letterGuessed);
-        console.log(GameBoard.wasGuessCorrect)
-        if(GameBoard.wasGuessCorrect){
-            
-            ViewEngine.updateWordToGuess();
+
+        if (GameBoard.wasGuessCorrect) {
+            AppController.handleCorrectGuess();
         }
-        else{
-           ViewEngine.updateNumberToGuess();
-
+        else {
+            AppController.handleIncorrectGuess();
         }
+    },
+    handleCorrectGuess: function () {
+        ViewEngine.updateWordToGuess();
+        GameBoard.didIWin();
+        console.log(GameBoard.wonGame);
+
 
     },
-    handleGuessIncorrect: function(){
+    handleIncorrectGuess: function () {
+        ViewEngine.updateNumberToGuess();
+        GameBoard.didILose();
+        console.log(GameBoard.lostGame);
+    },
 
+    handleWin: function () {
 
     },
-    
-    handleWin: function(){
-
-    },
-    handleLose: function(){
+    handleLose: function () {
 
     }
 }
 
-$(document).ready(function(){
+$(document).ready(function () {
 
-AppController.handleBoardSetUp();
-$('#a').on('click', AppController.handleMakeGuess);
-$('#b').on('click', AppController.handleMakeGuess);
-$('#c').on('click', AppController.handleMakeGuess);
-$('#d').on('click', AppController.handleMakeGuess);
-$('#e').on('click', AppController.handleMakeGuess);
-$('#f').on('click', AppController.handleMakeGuess);
-$('#g').on('click', AppController.handleMakeGuess);
-$('#h').on('click', AppController.handleMakeGuess);
-$('#i').on('click', AppController.handleMakeGuess);
-$('#j').on('click', AppController.handleMakeGuess);
-$('#k').on('click', AppController.handleMakeGuess);
-$('#l').on('click', AppController.handleMakeGuess);
-$('#m').on('click', AppController.handleMakeGuess);
-$('#n').on('click', AppController.handleMakeGuess);
-$('#o').on('click', AppController.handleMakeGuess);
-$('#p').on('click', AppController.handleMakeGuess);
-$('#q').on('click', AppController.handleMakeGuess);
-$('#r').on('click', AppController.handleMakeGuess);
-$('#s').on('click', AppController.handleMakeGuess);
-$('#t').on('click', AppController.handleMakeGuess);
-$('#u').on('click', AppController.handleMakeGuess);
-$('#v').on('click', AppController.handleMakeGuess);
-$('#w').on('click', AppController.handleMakeGuess);
-$('#x').on('click', AppController.handleMakeGuess);
-$('#y').on('click', AppController.handleMakeGuess);
-$('#z').on('click', AppController.handleMakeGuess);
+    AppController.handleBoardSetUp();
+    $('#a').on('click', AppController.handleMakeGuess);
+    $('#b').on('click', AppController.handleMakeGuess);
+    $('#c').on('click', AppController.handleMakeGuess);
+    $('#d').on('click', AppController.handleMakeGuess);
+    $('#e').on('click', AppController.handleMakeGuess);
+    $('#f').on('click', AppController.handleMakeGuess);
+    $('#g').on('click', AppController.handleMakeGuess);
+    $('#h').on('click', AppController.handleMakeGuess);
+    $('#i').on('click', AppController.handleMakeGuess);
+    $('#j').on('click', AppController.handleMakeGuess);
+    $('#k').on('click', AppController.handleMakeGuess);
+    $('#l').on('click', AppController.handleMakeGuess);
+    $('#m').on('click', AppController.handleMakeGuess);
+    $('#n').on('click', AppController.handleMakeGuess);
+    $('#o').on('click', AppController.handleMakeGuess);
+    $('#p').on('click', AppController.handleMakeGuess);
+    $('#q').on('click', AppController.handleMakeGuess);
+    $('#r').on('click', AppController.handleMakeGuess);
+    $('#s').on('click', AppController.handleMakeGuess);
+    $('#t').on('click', AppController.handleMakeGuess);
+    $('#u').on('click', AppController.handleMakeGuess);
+    $('#v').on('click', AppController.handleMakeGuess);
+    $('#w').on('click', AppController.handleMakeGuess);
+    $('#x').on('click', AppController.handleMakeGuess);
+    $('#y').on('click', AppController.handleMakeGuess);
+    $('#z').on('click', AppController.handleMakeGuess);
 
 });
